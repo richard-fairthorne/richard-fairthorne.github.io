2c6840ea438ace893fb22e2867dabdcd038d624e
Richard Fairthorne <richard.fairthorne@gmail.com>
2024-12-21 17:06:04 -0500

    reboots ok

---
+++ server/bootstrap/install.tcl
@@ -0,0 +1,29 @@
+#!/usr/bin/expect
+
+#Usage install <host>
+set timeout 300
+
+# Get host from command line
+set host [lindex $argv 0]
+if {$host == ""} {
+    puts "Usage: $argv0 <host>"
+    exit 1
+}
+
+# Read the ipxe.sh file content
+set ipxe_content [read [open "ipxe.sh" r]]
+
+# Connect via SSH
+spawn ssh $host
+
+send "\n"
+
+foreach command [split $ipxe_content "\n"] {
+	expect "iPXE> "
+	set timeout 15
+	foreach char [split $command ""] {
+		send -- $char
+		sleep 0.1
+	}
+    send "\n"
+}
---
+++ server/bootstrap/todo.md
@@ -0,0 +1,9 @@
+# booting ipxe
+
+- Attach serial console
+- Setup boot to ipxe
+- Reboot
+- Run install.tcl
+- Setup boot to drive
+- Reboot
+
